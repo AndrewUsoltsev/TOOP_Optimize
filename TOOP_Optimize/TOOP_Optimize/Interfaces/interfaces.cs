212636d38ebc2a4ffc1c1b4841e5063d9470e90b
AndrewUsoltsev <usol1996@gmail.com>
2019-02-09 18:42:28 +0700

    add folders and interfaces.

---
+++ TOOP_Optimize/TOOP_Optimize/Interfaces/interfaces.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOOP_Optimize.Interfaces
+{
+    interface Functional
+    {
+        double Value(double[] parameters);
+        (double min, double max)[] Range { get; }
+    }
+    interface FunctionalWithDiff : Functional
+    {
+        double DfDp(int i, double[] parameters);
+    }
+
+    interface Optimizer
+    {
+        Functional functional { set; }
+        double[] Optimize(double[] initial,
+            IProgress<(double[] current, double residual, int progresslen, int progressval)> progress);
+        double Eps { set; }
+        DateTime MaxTime { set; }
+    }
+
+}
