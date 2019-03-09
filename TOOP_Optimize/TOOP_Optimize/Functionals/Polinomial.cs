cdc734de172a2eafe0dbb0ad55f34ec42107ebd4
Margarita Golubeva <golubeva.rita2013@yandex.ru>
2019-03-09 20:21:56 +0700

    Completed a polynomial.

---
+++ TOOP_Optimize/TOOP_Optimize/Functionals/Polinomial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOOP_Optimize.Interfaces;
+
+namespace TOOP_Optimize.Functionals
+{
+    //y=a0+a1*x+...
+    class Polinomial : IFunctional
+    {
+        private List<Tuple<double, double>> listXY = new List<Tuple<double, double>>();
+        private double[] coeff;
+
+        public Polinomial(double[] coeff)
+        {
+            this.coeff = coeff;
+        }
+
+        public (double min, double max)[] Range => throw new NotImplementedException();
+
+        public double Value(double[] parameters)
+        {
+            return Polynomial(coeff, parameters[0]);
+        }
+
+        private double Polynomial(double[] coeff, double x)
+        {
+            var sum = 0.0;
+            var xBuf = 1.0;
+            var degree = coeff.Count();
+
+            for (int i = 0; i < degree; i++)
+            {
+                sum += coeff[i] * xBuf;
+                xBuf *= x;
+            }
+
+            return sum;
+        }
+    }
+}
