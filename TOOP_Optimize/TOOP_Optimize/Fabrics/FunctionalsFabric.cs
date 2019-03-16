f8bf341ff50f4d50470c3f66eec0158d6031d2b7
AndrewUsoltsev <usol1996@gmail.com>
2019-03-16 23:24:31 +0700

    fix 2

---
+++ TOOP_Optimize/TOOP_Optimize/Fabrics/FunctionalsFabric.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOOP_Optimize.Interfaces;
+using TOOP_Optimize.Functionals;
+
+namespace TOOP_Optimize.Fabrics
+{
+    public class FunctionalsFabric
+    {
+        public IFunctional GetPolinomialFunctional(double[] coeff)
+        {
+            return new Polinomial(coeff);
+        }
+
+
+    }
+}
---
+++ TOOP_Optimize/TOOP_Optimize/Fabrics/OptimizersFabric.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TOOP_Optimize.Interfaces;
+using TOOP_Optimize.Fabrics;
+
+namespace TOOP_Optimize.Fabrics
+{
+    public enum EOptimezerType
+    {
+
+    }
+
+    public class OptimizersFabric
+    {
+        public IOptimizer GetOptimizer(EOptimezerType type)
+        {
+
+        }
+    }
+}
