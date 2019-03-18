d6c5e5a4229a6d75b346e7629e0c71db50564158
Margarita Golubeva <golubeva.rita2013@yandex.ru>
2019-03-18 08:45:02 +0700

    Fix

--- TOOP_Optimize/TOOP_Optimize/Functionals/LSM.cs
+++ TOOP_Optimize/TOOP_Optimize/Functionals/LSM.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TOOP_Optimize.Interfaces;
 
 namespace TOOP_Optimize.Functionals
 {
-    class LSM
+    class LSM : IFunctional
     {
+        public (double min, double max)[] Range => throw new NotImplementedException();
+
+        public double Value(double[] parameters)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
