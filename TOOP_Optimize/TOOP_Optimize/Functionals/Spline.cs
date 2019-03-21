87c147a2d04c8b2eaf2d628e803d3ff40519d267
Margarita Golubeva <golubeva.rita2013@yandex.ru>
2019-03-21 23:15:15 +0700

    End spline.

--- TOOP_Optimize/TOOP_Optimize/Functionals/Spline.cs
+++ TOOP_Optimize/TOOP_Optimize/Functionals/Spline.cs
@@ -3,31 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using TOOP_Optimize.Interfaces;
 
 namespace TOOP_Optimize.Functionals
 {
     class Spline : IFunctional
     {
-        public (double min, double max)[] Range => throw new NotImplementedException();
+        private int sizeSpline;
+        private double Min;
+        private double Max;
+        private double[] nodesSpline;
+        private double[] valueSpline;
+
+        public Spline(int size, double min, double max, double[] nodes, double[] value)
+        {
+            Min = min;
+            Max = max;
+            sizeSpline = size;
+            nodesSpline = nodes;
+            valueSpline = value;
+            Range = new[] { (min, max) };
+        }
+
+        public (double min, double max)[] Range { get; }
 
         public double Value(double[] parameters)
         {
             return CalcFunctional(parameters);
         }
 
-        private int sizeSpline;
-        private double[] nodesSpline;
-        private double[] valueSpline;
-
-        public Spline(int size, double[] nodes, double[] value)
-        {
-            sizeSpline = size;
-            nodesSpline = nodes;
-            valueSpline = value;
-        }
-
-        private double ermit(int num, double x, double h)
+        private double ErmitBasicFunction(int num, double x, double h)
         {
             switch (num)
             {
@@ -43,54 +49,54 @@ namespace TOOP_Optimize.Functionals
             return 0;
         }
 
+        private double LinearBasicFunction(int num, double x, double x1, double h)
+        {
+            switch (num)
+            {
+                case 0:
+                    return (x1 - x) / h;
+                case 1:
+                    return (x - x1) / h;
+            }
+            return 0;
+        }
+
         private double CalcFunctional(double[] q)
         {
-            var numPoint = 0;
+            //var P = 0.0;
+            var functional = 0.0;
 
-            var A = new double[sizeSpline, sizeSpline];
-            var b = new double[sizeSpline];
             var w = new double[sizeSpline];
+            var n = q.Count();
 
             for (int i = 0; i < sizeSpline; i++)
             {
                 w[i] = 1.0;
             }
 
-            //Сборка А и б
-            for (int k = 1; k < sizeSpline - 1; k++)
+            var step_inter = (Max - Min) / n;
+            var x1 = Min;
+            var x2 = x1 + step_inter;
+            for (int i_inter = 0; i_inter < n - 1; i_inter++)
             {
-                double startInterval = nodesSpline[k], endInterval = nodesSpline[k + 1];
-                double h = endInterval - startInterval;
-                while (numPoint < sizeSpline && nodesSpline[numPoint] < endInterval)
+                for (int k = 0; k < sizeSpline; k++)
                 {
-                    double x = (nodesSpline[numPoint] - startInterval) / h;
-                    for (int i = 0; i < 4; i++)
+                    var x = nodesSpline[k];
+                    if (x1 <= x && x2 > x || i_inter == n - 2 && x2 == x)
                     {
-                        double psi_i = ermit(i, x, h);
-                        for (int j = 0; j < 4; j++)
-                        {
-                            A[k + i, k + j] += w[k + i] * psi_i * ermit(j, x, h);
-                        }
-                        b[k + i] += w[k + i] * psi_i * valueSpline[numPoint];
+                        var psi1 = LinearBasicFunction(0, x, x2, x2 - x1);
+                        var psi2 = LinearBasicFunction(1, x, x1, x2 - x1);
+                        var P = psi1 * q[i_inter] + psi2 * q[i_inter + 1];
+                        functional += w[k] * (P - valueSpline[k]) * (P - valueSpline[k]);
                     }
-                    numPoint++;
                 }
+                x1 += step_inter;
+                if (i_inter != n - 2)
+                    x2 += step_inter;
+                else
+                    x2 = Max;
             }
-
-
-            return 0;
-        }
-
-        private double[] MultVectorToMatrix(double[] vector, double[,] matrix)
-        {
-
-            return vector;
-        }
-
-        private double[] MultVectorToVector(double[] vectorFirst, double[] vectorSecond)
-        {
-
-            return vectorFirst;
+            return functional;
         }
     }
 }
