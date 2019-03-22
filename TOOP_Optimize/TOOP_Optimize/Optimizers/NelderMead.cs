d46b2adcd0c794acdb2b288b44c3ad34f62ccb56
Снетков Роман <snetkov.roman@gmail.com>
2019-03-22 09:43:59 +0700

    refactor

--- TOOP_Optimize/TOOP_Optimize/Optimizers/NelderMead.cs
+++ TOOP_Optimize/TOOP_Optimize/Optimizers/NelderMead.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Runtime.Remoting.Messaging;
 using System.Windows.Forms;
@@ -17,6 +18,8 @@ namespace TOOP_Optimize.Optimizers
             this.MaxTime = maxTime;
         }
 
+        public double Eps { get; set; }
+        public DateTime MaxTime { get; set; }
         public IFunctional functional { get; set; }
 
         const double alpha = 1;
@@ -24,15 +27,28 @@ namespace TOOP_Optimize.Optimizers
         const double rho = 0.5;
         const double sigma = 0.5;
 
+
         public double[] Optimize(double[] initial,
             IProgress<(double[] current, double residual, int progresslen, int progressval)> progress)
         {
+            if (initial == null)
+            {
+                throw new ArgumentNullException(nameof(initial));
+            }
+
+            if (progress == null)
+            {
+                throw new ArgumentNullException(nameof(progress));
+            }
+
+
             var residual = 0.0;
             var residualLastIter = 0.0;
             var lastVal = 0;
             double[] functionValues = new double[initial.Length + 1];
             var simplex = InitializeStartSimplex(initial);
             var N = initial.Length;
+
             while (true)
             {
                 for (int i = 0; i < simplex.GetLength(0); i++)
@@ -42,17 +58,7 @@ namespace TOOP_Optimize.Optimizers
 
                 Array.Sort(functionValues, simplex);
 
-                // Find centroid
-                double[] centroid = new double[initial.Length];
-                for (int i = 0; i < initial.Length; i++)
-                {
-                    for (int j = 0; j < N; j++)
-                    {
-                        centroid[i] += simplex[j][i];
-                    }
-
-                    centroid[i] /= N;
-                }
+                FindCentroid(simplex, N, out var centroid);
 
                 if (Math.Abs(residual) > Eps)
                 {
@@ -75,13 +81,7 @@ namespace TOOP_Optimize.Optimizers
                     progress.Report((simplex[0], residual, 100, lastVal));
 
 
-                double[] reflectionPoint = new double[initial.Length];
-                for (int i = 0; i < initial.Length; i++)
-                {
-                    reflectionPoint[i] = centroid[i] + alpha * (centroid[i] - simplex[N][i]);
-                }
-
-                double reflectionValue = functional.Value(reflectionPoint);
+                double reflectionValue = Reflection(N, centroid, simplex, out var reflectionPoint);
 
                 if (reflectionValue >= functionValues[0] && reflectionValue < functionValues[N - 1])
                 {
@@ -89,16 +89,9 @@ namespace TOOP_Optimize.Optimizers
                     continue;
                 }
 
-                // Expansion
                 if (reflectionValue < functionValues[0])
                 {
-                    double[] expansionPoint = new double[N];
-                    for (int i = 0; i < N; i++)
-                    {
-                        expansionPoint[i] = centroid[i] + gamma * (reflectionPoint[i] - centroid[i]);
-                    }
-
-                    double expansionValue = functional.Value(expansionPoint);
+                    double expansionValue = Expansion(N, centroid, simplex, reflectionPoint, out var expansionPoint);
 
                     if (expansionValue < reflectionValue)
                     {
@@ -112,14 +105,7 @@ namespace TOOP_Optimize.Optimizers
                     continue;
                 }
 
-                // Contraction
-                double[] contractionPoint = new double[N];
-                for (int i = 0; i < N; i++)
-                {
-                    contractionPoint[i] = centroid[i] + rho * (simplex[N][i] - centroid[i]);
-                }
-
-                double contractionValue = functional.Value(contractionPoint);
+                double contractionValue = Contraction(N, centroid, simplex, out var contractionPoint);
 
                 if (contractionValue < functionValues[N])
                 {
@@ -140,8 +126,53 @@ namespace TOOP_Optimize.Optimizers
             return simplex[0];
         }
 
-        public double Eps { get; set; }
-        public DateTime MaxTime { get; set; }
+        private void FindCentroid(double[][]simplex, int N, out double[] centroid)
+        {
+            centroid = new double[N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    centroid[i] += simplex[j][i];
+                }
+
+                centroid[i] /= N;
+            }
+        }
+
+        private double Reflection(int N, double[] centroid, double[][] simplex, out double[] reflectionPoint)
+        {
+            reflectionPoint = new double[N];
+            for (int i = 0; i < N; i++)
+            {
+                reflectionPoint[i] = centroid[i] + alpha * (centroid[i] - simplex[N][i]);
+            }
+
+            return functional.Value(reflectionPoint);
+        }
+
+        private double Expansion(int N, double[] centroid, double[][] simplex, 
+            double[] reflectionPoint, out double[] expansionPoint)
+        {
+            expansionPoint = new double[N];
+            for (int i = 0; i < N; i++)
+            {
+                expansionPoint[i] = centroid[i] + gamma * (reflectionPoint[i] - centroid[i]);
+            }
+
+            return functional.Value(expansionPoint);
+        }
+
+        private double Contraction(int N, double[] centroid, double[][] simplex, out double[] contractionPoint)
+        {
+            contractionPoint = new double[N];
+            for (int i = 0; i < N; i++)
+            {
+                contractionPoint[i] = centroid[i] + rho * (simplex[N][i] - centroid[i]);
+            }
+
+            return functional.Value(contractionPoint);
+        }
 
         private double[][] InitializeStartSimplex(double[] initial)
         {
@@ -163,8 +194,12 @@ namespace TOOP_Optimize.Optimizers
                 }
             }
 
-        return startSimplex;
+            return startSimplex;
         }
 
+        private bool AtRange(double[] point)
+        {
+            return functional.Range.Where((range, i) => point[i] < range.min || point[i] > range.max).Any();
+        }
     }
 }
